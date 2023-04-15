6fc1d18943efea9ca895b7ac11bfebfbcf687e01
Gary <garylin0969@gmail.com>
2023-04-15 15:45:45 +0800

    docs: add steps

--- src/App.tsx
+++ src/App.tsx
@@ -25,27 +25,34 @@ function App() {
                 .finally(() => setIsLoading(false));
         };
 
+        // root: 用於指定元素的根節點，若設為 null 則表示使用瀏覽器視窗作為根節點。
+        // rootMargin: 指定根節點的邊界，以像素為單位，這可以在觸發元素進入可視範圍之前預先增加一定的留白。
+        // threshold: 指定觸發回調函數的閾值，即當目標元素進入可視範圍的比例超過閾值時，就會觸發回調函數。
         const options = {
             root: null, // 根元素
             rootMargin: '0px', // 預留空間
             threshold: 0.5, // 監聽閾值
         };
 
-        const observer: IntersectionObserver = new IntersectionObserver((entries) => {  // 創建一個監聽物件，並傳入一個回調函數，當被觀察的元素進入可視範圍時，便會觸發回調函數
-            entries.forEach((entry) => {  // 遍歷觀察的元素，並判斷是否進入可視範圍
-                if (entry.isIntersecting) {  // isIntersecting 屬性，代表元素是否進入可視範圍
+        const observer: IntersectionObserver = new IntersectionObserver((entries) => {
+            // 創建一個監聽物件，並傳入一個回調函數，當被觀察的元素進入可視範圍時，便會觸發回調函數
+            entries.forEach((entry) => {
+                // 遍歷觀察的元素，並判斷是否進入可視範圍
+                if (entry.isIntersecting) {
+                    // isIntersecting 屬性，代表元素是否進入可視範圍
                     fetchPhotos();
                 }
             });
         }, options);
 
-        if (loaderRef.current) {  // 當 loaderRef.current 為真時，說明 loaderRef 已經有元素被指定
-            observer.observe(loaderRef.current);  // 通過監聽物件的 observe 方法，對 loaderRef.current 元素進行觀察
+        if (loaderRef.current) {
+            // 當 loaderRef.current 為真時，說明 loaderRef 已經有元素被指定
+            observer.observe(loaderRef.current); // 通過監聽物件的 observe 方法，對 loaderRef.current 元素進行觀察
         }
 
         return () => {
             if (loaderRef.current) {
-                observer.unobserve(loaderRef.current);  // 當組件銷毀時，使用監聽物件的 unobserve 方法，取消對 loaderRef.current 元素的觀察
+                observer.unobserve(loaderRef.current); // 當組件銷毀時，使用監聽物件的 unobserve 方法，取消對 loaderRef.current 元素的觀察
             }
         };
     }, []);
